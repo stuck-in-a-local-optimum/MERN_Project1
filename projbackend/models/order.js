44f69eff8b93f416ee531b657ece3d17e694c275
Ajeet Yadav <ajeet19010@iiitd.ac.in>
2021-06-06 17:52:43 +0530

    Update order.js

--- projbackend/models/order.js
+++ projbackend/models/order.js
@@ -5,7 +5,9 @@ const {ObjectId} = mongoose.Schema;
 
 //Multiple schemas in one file
 
-const ProductCartSchema = new mongoose.Schema({
+
+//when the product is in cart it has some other properties like quanity, so we need a new schema for it
+const ProductCartSchema = new mongoose.Schema({  
     product :{
         type: ObjectId,
         ref: "Product"
@@ -26,7 +28,7 @@ const orderSchema = new mongoose.Schema(
     updated: Date,
     user: {
         type:ObjectId,
-        ref: "User",   //same way that we did in category file
+        ref: "User",   //same way that we did in category file  //this user will be populated by populate method in mongoose
     }
 },
 
