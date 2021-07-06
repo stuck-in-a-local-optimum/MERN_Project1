7f6ccc39a1d12df9619306448c6cc2ca1cbbe7d0
Ajeet Yadav <ajeet19010@iiitd.ac.in>
2021-07-06 22:51:47 +0530

    user dashboard

---
+++ projfrontend/src/user/UserDashBoard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Base from "../core/Base"
+
+
+const UserDashBoard = () =>{
+    return(
+        <Base title="UserDashBoard Page">
+            <h1>This is user dashboard page</h1>
+
+        </Base>
+
+    )
+
+}
+
+export default UserDashBoard;
