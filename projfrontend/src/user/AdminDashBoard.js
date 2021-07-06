b839719ca6e49d42caa7d4d3a7c1810f7156288d
Ajeet Yadav <ajeet19010@iiitd.ac.in>
2021-07-06 22:51:37 +0530

    Admin DashBoard

---
+++ projfrontend/src/user/AdminDashBoard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Base from "../core/Base"
+
+
+const AdminDashBoard = () =>{
+    return(
+        <Base title="AdminDashBoard Page">
+            <h1>This is Admin dashboard page</h1>
+
+        </Base>
+
+    )
+
+}
+
+export default AdminDashBoard;
